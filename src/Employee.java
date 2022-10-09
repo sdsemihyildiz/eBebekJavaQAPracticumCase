0054594cc82f9381a941b924292b4af54f863952
SDSemoch <sdsemihyildiz@gmail.com>
2022-10-09 17:19:27 +0300

    last update
    added readme
    fixed raise salary

--- src/Employee.java
+++ src/Employee.java
@@ -12,32 +12,33 @@ public class Employee {
     }
 
     public double tax() throws Exception {
-        if (this.salary < 1000.0 && this.salary > 0.0) {
+        if (salary < 1000.0 && salary > 0.0) {
             return 0.0;
-        } else if (this.salary >= 1000.0) {
-            return this.salary * 0.03;
+        } else if (salary >= 1000.0) {
+            return salary * 0.03;
         } else {
             throw new Exception("Invalid salary.");
         }
     }
 
     public int bonus() throws Exception {
-        if (this.workHours <= 40 && this.workHours >= 0) {
+        if (workHours <= 40 && workHours >= 0) {
             return 0;
-        } else if (this.workHours > 40) {
-            return (this.workHours - 40) * 30;
+        } else if (workHours > 40) {
+            return (workHours - 40) * 30;
         } else {
             throw new Exception("Invalid work hours");
         }
     }
 
     public double raiseSalary() throws Exception {
-        if (2021 - this.hireYear < 10 && 2021 - this.hireYear >= 0) {
-            return this.salary * 0.05;
-        } else if (2021 - this.hireYear > 9 && 2021 - this.hireYear < 20) {
-            return this.salary * 0.1;
-        } else if (2021 - this.hireYear > 19) {
-            return this.salary * 0.15;
+
+        if (2021 - hireYear < 10 && 2021 - hireYear >= 0) {
+            return (this.salary + bonus() - tax()) * 0.05;
+        } else if (2021 - hireYear > 9 && 2021 - hireYear < 20) {
+            return (this.salary + bonus() - tax()) * 0.1;
+        } else if (2021 - hireYear > 19) {
+            return (this.salary + bonus() - tax()) * 0.15;
         } else {
             throw new Exception("Invalid hire year.");
         }
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        Employee e1 = new Employee("Semih Yildiz", 1000.0, 45, 2021);
+        //Kontrol amaçlı oluşturulmuş bir main
 
+        Employee e1 = new Employee("Semih Yildiz", 16500, 45, 2010);
+
+        System.out.println(e1);
         System.out.println("Tax amount: " + e1.tax());
-        System.out.println("Salary Raise: " + e1.raiseSalary());
-        System.out.println(e1.toString());
         System.out.println("Salary Bonus: " + e1.bonus());
+        System.out.println("Salary Raise: " + e1.raiseSalary());
 
     }
 }
